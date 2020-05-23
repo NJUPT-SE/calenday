4dcd3198238c7e58ec81f4b213b788c41b9685d5
Yifei Zhou <897701499@qq.com>
2020-05-23 22:52:03 +0800

    后端【登录、纪念日】

---
+++ backend/src/main/java/com/wx/Controller/Big_Day_Controller.java
@@ -0,0 +1,341 @@
+package com.wx.Controller;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.*;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+@RestController
+@SpringBootApplication
+@org.springframework.stereotype.Controller
+public class Big_Day_Controller {
+
+    //新增一条纪念日记录
+    //接受从前端传来的uid,title,date,notes,img存入数据库
+    //新建成功返回err=1，失败err=0
+    //url:http://localhost:8080/api/bigDay/build
+    @RequestMapping("api/bigDay/build")
+    @ResponseBody
+    public Map<String, Object> bigday_build(Integer uid, String title,String date, String notes,Integer img)
+            throws  ParseException {
+
+        int err=0; //新建成功，err=1，新建失败，err=0
+
+        //获取的String类型date转化为java.sql.Date类型
+        DateFormat format1 = new SimpleDateFormat("yyyy-MM-dd");
+        java.util.Date date2 = null;
+        date2 =format1.parse(date);
+        java.sql.Date date3 = new java.sql.Date(date2.getTime());
+
+        //数据库部分
+        final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+        final String DB_URL = "jdbc:mysql://localhost:3306/user";
+        //数据库的用户名与密码，需要根据自己的设置
+        final String USER = "root";
+        final String PASS = "078406aaa";
+
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            // 注册JDBC驱动
+            Class.forName(JDBC_DRIVER);
+
+            System.out.println("连接数据库...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            stmt = conn.createStatement();
+            String sql;
+
+            //核心部分
+            //为bigday分配id
+            sql = "SELECT * FROM bigday";
+            ResultSet rs = stmt.executeQuery(sql);
+            int max_id=0;
+            while (rs.next()) {
+                if(uid==rs.getInt("uid")) {
+                    int current_id = rs.getInt("id");
+                    if (max_id < current_id) {
+                        max_id = current_id;
+                    }
+                }
+            }
+            int id=max_id+1;
+
+            //写入数据库完成新建
+            if (err==0) {
+                try {
+                    stmt = conn.prepareStatement("insert into bigday (uid,id,title,date,notes,img) values(?,?,?,?,?,?)");
+                    ((PreparedStatement) stmt).setInt(1, uid);
+                    ((PreparedStatement) stmt).setInt(2, id);
+                    ((PreparedStatement) stmt).setString(3, title);
+                    ((PreparedStatement) stmt).setDate(4, date3);
+                    ((PreparedStatement) stmt).setString(5, notes);
+                    ((PreparedStatement) stmt).setInt(6, img);
+                    ((PreparedStatement) stmt).executeUpdate();
+                    err=1;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            // 完成后关闭
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            // 处理 JDBC 错误
+            se.printStackTrace();
+        } catch (Exception e) {
+            // 处理 Class.forName 错误
+            e.printStackTrace();
+        } finally {
+            // 关闭资源
+            try {
+                if (stmt != null) stmt.close();
+            } catch (SQLException se2) {
+            }// 什么都不做
+            try {
+                if (conn != null) conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+        //返回err
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("err", err);
+        return map;
+
+    }
+
+
+    //删除一条纪念日记录
+    //接受从前端传来的uid,id在数据库中查找相应纪念日记录并删除
+    //删除成功返回err=1，失败err=0
+    //url:http://localhost:8080/api/bigDay/delete
+    @RequestMapping("api/bigDay/delete")
+    @ResponseBody
+    public Map<String, Object> bigday_delete(int uid, int id) {
+
+        int err=0; //删除成功，err=1，删除失败，err=0
+
+        //数据库部分
+        final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+        final String DB_URL = "jdbc:mysql://localhost:3306/user";
+        //数据库的用户名与密码，需要根据自己的设置
+        final String USER = "root";
+        final String PASS = "078406aaa";
+
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            // 注册JDBC驱动
+            Class.forName(JDBC_DRIVER);
+
+            System.out.println("连接数据库...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            //核心部分
+            //根据uid，id找到纪念日记录并删除
+            stmt = conn.createStatement();
+            String sql;
+            sql = "DELETE FROM bigday WHERE id="+id+" AND uid="+uid;
+            //System.out.println(sql);
+            stmt.executeUpdate(sql);
+            err=1;
+
+            // 完成后关闭
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            // 处理 JDBC 错误
+            se.printStackTrace();
+        } catch (Exception e) {
+            // 处理 Class.forName 错误
+            e.printStackTrace();
+        } finally {
+            // 关闭资源
+            try {
+                if (stmt != null) stmt.close();
+            } catch (SQLException se2) {
+            }// 什么都不做
+            try {
+                if (conn != null) conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+        //返回err
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("err", err);
+        return map;
+    }
+
+
+    //查看用户的全部纪念日记录
+    //接受从前端传来的uid在数据库中查找所有纪念日记录
+    //按照date升序(2020-05-11,2020-05-12,2020-05-13)返回所有纪念日记录的id、title、date、notes、img
+    //查看成功返回err=1，失败err=0
+    //url:http://localhost:8080/api/bigDay/view
+    @RequestMapping("api/bigDay/view")
+    @ResponseBody
+    public Map<String, Object> bigday_view(int uid) {
+
+        int err=0; //删除成功，err=1，删除失败，err=0
+        SimpleDateFormat f= new SimpleDateFormat("yyyy-MM-dd");
+
+        //ArrayList，以Record类型来临时存储用户所有记录
+        ArrayList<HashMap> all_record=new ArrayList<>();
+
+        //数据库部分
+        final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+        final String DB_URL = "jdbc:mysql://localhost:3306/user";
+        //数据库的用户名与密码，需要根据自己的设置
+        final String USER = "root";
+        final String PASS = "078406aaa";
+
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            // 注册JDBC驱动
+            Class.forName(JDBC_DRIVER);
+
+            System.out.println("连接数据库...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT * FROM bigday ORDER BY date ASC";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            //根据date
+
+            //根据uid找到所有纪念日记录并储存在ArrayList中
+            while (rs.next()) {
+                if(uid==rs.getInt("uid")) {
+
+                    HashMap<String, Object> map1 = new HashMap<String, Object>();
+
+                    map1.put("id", rs.getInt("id"));
+                    map1.put("title",rs.getString("title"));
+                    java.sql.Date current_date=rs.getDate("date");
+                    String current_date2=f.format(current_date);        //java.sql.Date转成String
+                    map1.put("date",current_date2);
+                    map1.put("notes",rs.getString("notes"));
+                    map1.put("img",rs.getInt("img"));
+
+                    all_record.add(map1);
+                }
+            }
+            err=1;
+
+            // 完成后关闭
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            // 处理 JDBC 错误
+            se.printStackTrace();
+        } catch (Exception e) {
+            // 处理 Class.forName 错误
+            e.printStackTrace();
+        } finally {
+            // 关闭资源
+            try {
+                if (stmt != null) stmt.close();
+            } catch (SQLException se2) {
+            }// 什么都不做
+            try {
+                if (conn != null) conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+        //返回数组中的数据
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("err", err);
+        map.put("data",all_record);
+        //System.out.println(all_record);
+        return map;
+
+    }
+
+
+    //修改用户的一条纪念日记录
+    //接受从前端传来的uid、id、title、date、notes、img，根据uid、id定位记录并更新
+    //修改成功返回err=1,失败err=0
+    //url:http://localhost:8080/api/bigDay/revise
+    @RequestMapping("api/bigDay/revise")
+    @ResponseBody
+    public Map<String, Object> bigday_revise(Integer uid,Integer id, String title,String date, String notes,Integer img)
+            throws  ParseException {
+
+        int err=0; //修改成功，err=1，修改失败，err=0
+
+        //获取的String类型date转化为java.sql.Date类型
+        DateFormat format1 = new SimpleDateFormat("yyyy-MM-dd");
+        java.util.Date date2 = null;
+        date2 =format1.parse(date);
+        java.sql.Date date3 = new java.sql.Date(date2.getTime());
+
+        //数据库部分
+        final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+        final String DB_URL = "jdbc:mysql://localhost:3306/user";
+        //数据库的用户名与密码，需要根据自己的设置
+        final String USER = "root";
+        final String PASS = "078406aaa";
+
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        try {
+            // 注册JDBC驱动
+            Class.forName(JDBC_DRIVER);
+
+            System.out.println("连接数据库...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            String sql;
+            sql= "update bigday set title=?,date=?,notes=?,img=? where uid=? and id=?";
+            stmt=conn.prepareStatement(sql);
+            stmt.setString(1,title);
+            stmt.setDate(2,date3);
+            stmt.setString(3,notes);
+            stmt.setInt(4,img);
+            stmt.setInt(5,uid);
+            stmt.setInt(6,id);
+            stmt.executeUpdate();
+            err=1;
+
+            // 完成后关闭
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            // 处理 JDBC 错误
+            se.printStackTrace();
+        } catch (Exception e) {
+            // 处理 Class.forName 错误
+            e.printStackTrace();
+        } finally {
+            // 关闭资源
+            try {
+                if (stmt != null) stmt.close();
+            } catch (SQLException se2) {
+            }// 什么都不做
+            try {
+                if (conn != null) conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+        //返回err
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("err", err);
+        return map;
+    }
+
+}
+
---
+++ backend/src/main/java/com/wx/Controller/User_Manage_Controller.java
@@ -0,0 +1,181 @@
+package com.wx.Controller;
+
+import net.sf.json.JSONObject;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.sql.*;
+import java.util.*;
+
+@RestController
+@SpringBootApplication
+@org.springframework.stereotype.Controller
+public class User_Manage_Controller {
+
+    //接受从前端传来的code（code）、性别（gender）、用户昵称（nickname）、头像url（avaUrl）
+    //根据uid查询数据库判断是否注册，若未注册，则将用户信息写入数据库
+    //已注册flag返回1，未注册flag返回0,并返回用户uid
+    //url : http://localhost:8080/api/UserManage
+    @RequestMapping("api/UserManage")
+    @ResponseBody
+    public Map<String, Object> user_manage(String code, String gender, String nickname, String avaUrl)
+            throws SQLException {
+        System.out.println("微信小程序正在调用...");
+
+        String flag = "0";  //判断用户注册or登录，0代表注册，1代表登录
+        int uid=0;  //用户uid，为自增长的主键
+
+        //POST获取用户openid
+        String url="https://api.weixin.qq.com/sns/jscode2session";
+        String realurl=url+"?"+"appid=wx1c1d22415dd9aa90&secret=93298a938c8363cab3b408b5f0bbd956&js_code="
+                +code+"&grant_type=authorization_code";
+        String R=sendGet(realurl);
+
+        //提取openid
+        JSONObject object=JSONObject.fromObject(R);
+        String openid= (String) object.get("openid");
+        System.out.println("openid:"+openid);
+
+        //数据库部分
+        final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+        final String DB_URL = "jdbc:mysql://localhost:3306/user";
+        //数据库的用户名与密码，需要根据自己的设置
+        final String USER = "root";
+        final String PASS = "078406aaa";
+
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            // 注册JDBC驱动
+            Class.forName(JDBC_DRIVER);
+
+            System.out.println("连接数据库...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            stmt = conn.createStatement();
+            String sql1,sql2;
+
+            //检索数据库中是否有该openid
+            sql1 = "SELECT openid FROM tb1";
+            ResultSet rs1 = stmt.executeQuery(sql1);
+            while (rs1.next()) {
+                String exist_openid_1 = rs1.getString("openid");
+                if (openid.equals(exist_openid_1)) {
+                    flag = "1";
+                    System.out.println("登录成功");
+                    break;
+                }
+            }
+
+            //数据库中没有该openid，则写入数据库完成注册
+            if (flag.equals("0")) {
+                try {
+                    stmt = conn.prepareStatement("insert into tb1 (openid,gender,nickname,avaUrl) values(?,?,?,?)");
+                    ((PreparedStatement) stmt).setString(1, openid);
+                    ((PreparedStatement) stmt).setString(2, gender);
+                    ((PreparedStatement) stmt).setString(3, nickname);
+                    ((PreparedStatement) stmt).setString(4, avaUrl);
+                    ((PreparedStatement) stmt).executeUpdate();
+                    System.out.println("注册成功");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            //根据openid获取uid
+            sql2="SELECT * FROM tb1";
+            ResultSet rs2 = stmt.executeQuery(sql2);
+            while (rs2.next()) {
+                String exist_openid_2 = rs2.getString("openid");
+                if (openid.equals(exist_openid_2)) {
+                    uid=rs2.getInt("uid");
+                    break;
+                }
+            }
+
+            // 完成后关闭
+            rs1.close();
+            rs2.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            // 处理 JDBC 错误
+            se.printStackTrace();
+        } catch (Exception e) {
+            // 处理 Class.forName 错误
+            e.printStackTrace();
+        } finally {
+            // 关闭资源
+            try {
+                if (stmt != null) stmt.close();
+            } catch (SQLException se2) {
+            }// 什么都不做
+            try {
+                if (conn != null) conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+
+        Map<String, Object> map = new HashMap<String, Object>();
+
+        System.out.println("微信小程序调用完成...");
+        map.put("uid",uid);
+        map.put("flag", flag);
+        return map;
+    }
+
+    //GET请求
+    public static String sendGet(String url) {
+        String result = "";
+        BufferedReader in = null;
+        try {
+            URL realUrl = new URL(url);
+            if("https".equalsIgnoreCase(realUrl.getProtocol())){
+                SslUtils.ignoreSsl();
+            }
+            // 打开和URL之间的连接
+            URLConnection connection = realUrl.openConnection();
+            // 设置通用的请求属性
+            connection.setRequestProperty("accept", "*/*");
+            connection.setRequestProperty("connection", "Keep-Alive");
+            connection.setRequestProperty("user-agent",
+                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            // 建立实际的连接
+            connection.connect();
+            // 获取所有响应头字段
+            Map<String, List<String>> map = connection.getHeaderFields();
+            // 遍历所有的响应头字段
+            for (String key : map.keySet()) {
+                System.out.println(key + "--->" + map.get(key));
+            }
+            // 定义 BufferedReader输入流来读取URL的响应
+            in = new BufferedReader(new InputStreamReader(
+                    connection.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+        } catch (Exception e) {
+            System.out.println("发送GET请求出现异常！" + e);
+            e.printStackTrace();
+        }
+        // 使用finally块来关闭输入流
+        finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        return result;
+    }
+}
